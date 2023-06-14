2f0bcdee113106e157e1b2dd78a38bb22c016cee
John Tian <85849926+snugpenguin968@users.noreply.github.com>
2023-06-13 20:31:47 -0500

    add resume, restart, and pause buttons; allow toggling

--- src/GamePage.js
+++ src/GamePage.js
@@ -2,6 +2,8 @@ import './GamePage.css';
 import Cell from './Cell.js'
 import NextBlock from './NextBlock.js'
 import ScoreBoard from './ScoreBoard.js'
+import Popup from './Popup.js'
+import {useState} from 'react';
 function Board(){
     const cells=[];
     for(let i=0;i<20;i++){
@@ -15,13 +17,33 @@ function Board(){
         </div>
     );
 }
+function Button(props){
+    if(props.id==="resume"&&!props.shouldResume){
+        return null;
+    }
+    if(props.id==="pause"&&props.shouldResume){
+        return null;
+    }
+    return <button className="meta" id={props.id} onClick={props.onClick}>{props.text}</button>
+}
 function Page(){
+    const [resume,shouldShowResume]=useState(false);
+    function setResume(){
+        shouldShowResume(!resume);
+    }
+    function restartGame(){
+        shouldShowResume(false);
+    }
     return (
         <div className="container">
             <div className="side-area">
                 <NextBlock></NextBlock>
                 <ScoreBoard></ScoreBoard>
+                <Button className="meta" id="pause" onClick={setResume} shouldResume={resume} text="Pause">Pause</Button>
+                <Button className="meta" id="resume" onClick={setResume} shouldResume={resume} text="Resume">Resume</Button>
+                <Button className="meta" id="restart" onClick={restartGame} shouldResume={resume} text="Restart">Restart</Button>
             </div>
+            
             <Board></Board>
         </div>
     );
---
+++ src/Popup.js
@@ -0,0 +1,8 @@
+export default function Popup(props){
+    return(
+        <div className="popup">
+            <h1>Message Title</h1>
+            <p>Message info...</p>
+        </div>
+    );
+}
