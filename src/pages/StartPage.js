7d26005ee5d057412f989c0d30682ea71f890dd8
John Tian <85849926+snugpenguin968@users.noreply.github.com>
2023-06-20 20:37:49 -0500

    add multiplayer button to start page

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,12 +1,14 @@
 import {Link} from 'react-router-dom'
+import '../GamePage.css';
 export default function StartMenu(){
     return(
         <div className="start-container">
             <div className="start-menu">
                 <h1 className="title">Tetris</h1>
-                <Link to='/Play'>
-                    <button className="play-button">Play</button>
+                <Link className="buttons" to='/Play'>
+                    <button className="play-button">Singleplayer</button>
                 </Link>
+                <button className="play-button">Multiplayer</button>
             </div>
         </div>
     );
