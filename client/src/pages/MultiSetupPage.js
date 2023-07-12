38c70a325efd1f53fcc84ed2311eacc70621d065
John Tian <85849926+snugpenguin968@users.noreply.github.com>
2023-07-11 21:43:36 -0500

    allow 2 players to join same room

--- client/src/pages/MultiSetupPage.js
+++ client/src/pages/MultiSetupPage.js
@@ -1,7 +1,78 @@
 import '../GamePage.css';
-import {useState} from "react";
+import { useEffect, useState, useCallback } from "react";
 import socket from '../socket'
 import {Link} from 'react-router-dom'
+
+function JoinRoom({setRoom,setPlayers}){
+    const [roomInput, setRoomInput] = useState('');
+    const [roomError, setRoomError] = useState('');
+    const handleIDChange = (e) => {
+      setRoomInput(e.target.value);
+    };
+  
+    const handleSubmit = () => {
+      if (!roomInput) {
+        return;
+      }
+      socket.emit("joinRoom",{roomId:roomInput},(r)=>{
+        if(r.error){
+            return setRoomError(r.messgae);
+        }
+        console.log("response:",r);
+        setRoom(r?.roomId);
+        setPlayers(r?.players);
+      })
+    }
+  return (
+      <div className="join-container inactive">
+          <h2>Enter a valid room ID</h2>
+          <form className="id-form">
+              <input
+                  type="text"
+                  id="id"
+                  name="id"
+                  className="id-input"
+                  onChange={handleIDChange}
+                  required
+              />
+              <br />
+              <Link to={roomInput ? "/Play" : ""}>
+                    <input
+                        type="button"
+                        value="Submit"
+                        className="id-button"
+                        onClick={handleSubmit}
+                        />
+              </Link>
+          </form>            
+      </div>
+  );
+}
+
+function GameRoom({setRoom}){
+    return (
+        <div className="gameroom-container inactive">
+            <Link to= "/Play">
+                <button className="start-game" onClick={
+                    ()=>{
+                        socket.emit("createRoom",(r)=>{
+                            console.log(r);
+                            setRoom(r);
+                        })
+                    }
+                }>Start a Game</button>
+            </Link>
+            <button className="start-game" onClick={
+                ()=>{
+                    const gameroomContainer=document.querySelector('.gameroom-container');
+                    gameroomContainer.classList.add('inactive');
+                    const joinContainer=document.querySelector('.join-container');
+                    joinContainer.classList.remove('inactive');
+                }
+            }>Join a Game</button>
+        </div>
+    );
+}
 function Dialogue(){
     const [username, setUsername] = useState('');
     const handleUsernameChange = (e) => {
@@ -13,6 +84,10 @@ function Dialogue(){
           return;
         }
         socket.emit("username", username);
+        const dialogueContainer=document.querySelector('.dialogue-container');
+        dialogueContainer.classList.add('inactive');
+        const gameroomContainer=document.querySelector('.gameroom-container');
+        gameroomContainer.classList.remove('inactive');
       };
     return (
         <div className="dialogue-container">
@@ -29,22 +104,39 @@ function Dialogue(){
                     required
                 />
                 <br />
-                <Link to={username ? "/Play" : ""}>
-                    <input
+                <input
                     type="button"
                     value="Submit"
                     className="username-button"
                     onClick={handleSubmit}
                     />
-                </Link>
             </form>
         </div>
     );
 }
 export default function MultiSetupPage(){
+    const [room, setRoom] = useState("");
+    const [orientation, setOrientation] = useState("");
+    const [players, setPlayers] = useState([]);
+
+    const cleanup = useCallback(() => {
+        setRoom("");
+        setOrientation("");
+        setPlayers("");
+      }, []);
+    
+    useEffect(() => {
+        socket.on("opponentJoined", (roomData) => {
+            console.log("roomData", roomData)
+            setPlayers(roomData.players);
+        });
+    }, []);
+
     return (
         <div className="container">
             <Dialogue></Dialogue>
+            <GameRoom setRoom={setRoom}></GameRoom>
+            <JoinRoom setRoom={setRoom} setPlayers={setPlayers}></JoinRoom>
         </div>
     );
 }
--- server/server.js
+++ server/server.js
@@ -12,12 +12,70 @@ const io=new Server(server,{
     cors:'*'
 });
 
+const rooms=new Map();
+
 io.on('connection',(socket)=>{
     console.log(socket.id,'connected');
     socket.on('username',(username)=>{
         console.log('username:',username);
         socket.data.username=username;
     })
+    socket.on('createRoom',async(callback)=>{
+        const roomId=uuidV4();
+        await socket.join(roomId);
+
+        rooms.set(roomId,{
+            roomId,
+            players:[{id:socket.id,username:socket.data?.username}]
+        })
+        callback(roomId);
+    })
+    socket.on('joinRoom', async (args, callback) => {
+        const room = rooms.get(args.roomId);
+        let error, message;
+      
+        if (!room) { // if room does not exist
+          error = true;
+          message = 'room does not exist';
+        } else if (room.length <= 0) { // if room is empty set appropriate message
+          error = true;
+          message = 'room is empty';
+        } else if (room.length >= 2) { // if room is full
+          error = true;
+          message = 'room is full'; // set message to 'room is full'
+        }
+    
+        if (error) {
+          // if there's an error, check if the client passed a callback,
+          // call the callback (if it exists) with an error object and exit or 
+          // just exit if the callback is not given
+    
+          if (callback) { // if user passed a callback, call it with an error payload
+            callback({
+              error,
+              message
+            });
+          }
+    
+          return; // exit
+        }
+        await socket.join(args.roomId); // make the joining client join the room
+        // add the joining user's data to the list of players in the room
+        const roomUpdate = {
+          ...room,
+          players: [
+            ...room.players,
+            { id: socket.id, username: socket.data?.username },
+          ],
+        };
+    
+        rooms.set(args.roomId, roomUpdate);
+    
+        callback(roomUpdate); // respond to the client with the room details.
+    
+        // emit an 'opponentJoined' event to the room to tell the other player that an opponent has joined
+        socket.to(args.roomId).emit('opponentJoined', roomUpdate);
+      });
 })
 
 server.listen(port, () => {
